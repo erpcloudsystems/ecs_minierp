fe3b51d6f5c986987628815c83ce8ebd8aedf060
mghoneem10 <mghoneem10@gmail.com>
2022-05-08 11:37:49 +0000

    8-5-2022

---
+++ ecs_minierp/ecs_minierp/doctype/cash_entry/cash_entry.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2022, ERPCloud.Systems and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class CashEntry(Document):
+	@frappe.whitelist()
+	def validate(self):
+		self.total_amount = 0
+		for x in self.expense_entry_account:
+			self.total_amount += x.amount
+
+	@frappe.whitelist()
+	def before_submit(self):
+		if self.total_amount != self.amount:
+			frappe.throw(" Amount Must Be Equal To Total Amount Of Accounts Table ... Difference is " + str(self.amount - self.total_amount))
+
+	@frappe.whitelist()
+	def on_submit(self):
+		if self.payment_type == "Pay":
+			accounts = [
+				{
+					"doctype": "Journal Entry Account",
+					"account": self.account_paid_from,
+					"credit": self.amount,
+					"debit": 0,
+					"credit_in_account_currency": self.amount
+				}
+			]
+
+			for x in self.expense_entry_account:
+				accounts1 = {
+					"doctype": "Journal Entry Account",
+					"account": x.account,
+					"credit": 0,
+					"debit": x.amount,
+					"debit_in_account_currency": x.amount,
+					"party_type": x.party_type,
+					"party": x.party,
+					"cost_center": x.cost_center,
+					"branch": x.branch,
+					"project": x.project,
+					"user_remark": x.user_remark
+				},
+				accounts.extend(accounts1)
+
+			new_doc = frappe.get_doc({
+				"doctype": "Journal Entry",
+				"voucher_type": "Journal Entry",
+				"reference_doctype": "Cash Entry",
+				"reference_link": self.name,
+				"cheque_no": self.name,
+				"cheque_date": self.posting_date,
+				"posting_date": self.posting_date,
+				"accounts": accounts,
+				"payment_type": self.payment_type,
+				"company": self.company,
+				"user_remark": self.remarks
+			})
+
+			new_doc.insert(ignore_permissions=True)
+			new_doc.submit()
+
+		if self.payment_type == "Receive":
+			accounts = [
+				{
+					"doctype": "Journal Entry Account",
+					"account": self.account_paid_to,
+					"credit": 0,
+					"debit": self.amount,
+					"debit_in_account_currency": self.amount				}
+			]
+
+			for x in self.expense_entry_account:
+				accounts1 = {
+								"doctype": "Journal Entry Account",
+								"account": x.account,
+								"credit": x.amount,
+								"debit": 0,
+								"credit_in_account_currency": x.amount,
+								"party_type": x.party_type,
+								"party": x.party,
+								"cost_center": x.cost_center,
+								"branch": x.branch,
+								"project": x.project,
+								"user_remark": x.user_remark
+							},
+				accounts.extend(accounts1)
+
+			new_doc = frappe.get_doc({
+				"doctype": "Journal Entry",
+				"voucher_type": "Journal Entry",
+				"reference_doctype": "Cash Entry",
+				"reference_link": self.name,
+				"cheque_no": self.name,
+				"cheque_date": self.posting_date,
+				"posting_date": self.posting_date,
+				"accounts": accounts,
+				"payment_type": self.payment_type,
+				"company": self.company,
+				"user_remark": self.remarks
+			})
+
+			new_doc.insert(ignore_permissions=True)
+			new_doc.submit()
